df65484419037ddec428528fb9adecffaf32b867
한성길 <zaien24@gmail.com>
2025-04-14 16:03:29 +0900

    4강. 사칙연산 계산기에 대해 테스트 코드 작성하기_4 테스트 2와 동일

--- src/main/kotlin/com/work/falibrary/calculator/Calculator.kt
+++ src/main/kotlin/com/work/falibrary/calculator/Calculator.kt
@@ -1,29 +1,24 @@
 import java.lang.IllegalArgumentException
 
 class Calculator(
-    private var _number: Int
+    var number: Int
 ) {
-
-    val number: Int
-        get() = this._number
-
-
     fun add(operand: Int) {
-        this._number += operand
+        this.number += operand
     }
 
     fun minus(operand: Int) {
-        this._number -= operand
+        this.number -= operand
     }
 
     fun multiply(operand: Int) {
-        this._number *= operand
+        this.number *= operand
     }
 
     fun divide(operand: Int) {
         if (operand == 0) {
             throw IllegalArgumentException("0으로 나눌 수 없습니다")
         }
-        this._number /= operand
+        this.number /= operand
     }
 }
--- src/test/kotlin/com/work/falibrary/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/work/falibrary/calculator/CalculatorTest.kt
@@ -10,9 +10,13 @@ fun main() {
 class CalculatorTest {
 
     fun addTest() {
+        // given
         val calculator = Calculator(5)
+
+        // when
         calculator.add(3)
 
+        // then
         if (calculator.number != 8) {
             throw IllegalStateException()
         }
